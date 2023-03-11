7938b7162aca7be22685e7532328fc277e70943f
Anton Meshchaninov <tonymsnv@gmail.com>
2023-03-11 23:57:32 +0300

    fixed order feed

--- src/components/OrderFeed/OrderFeed.tsx
+++ src/components/OrderFeed/OrderFeed.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 // Redux
 import { useSelector } from 'redux/store'
 import { webSocketOrdersSelector } from 'redux/selectors'
@@ -8,13 +10,13 @@ import { OrderCard } from 'components'
 // Styles
 import styles from './OrderFeed.module.scss'
 
-const OrderFeed = () => {
+const OrderFeed: React.FC = () => {
   const orders = useSelector(webSocketOrdersSelector)
 
   return (
     <section className={styles.wrapper}>
       <ul className={styles.list}>
-        {orders.map(el => (
+        {orders.slice(0, 12).map(el => (
           <OrderCard key={el._id} order={el} />
         ))}
       </ul>
@@ -22,4 +24,4 @@ const OrderFeed = () => {
   )
 }
 
-export default OrderFeed
+export default React.memo(OrderFeed)
